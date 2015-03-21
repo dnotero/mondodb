7d346660ecbf4ccb7d8e1d167559b7804b1c9553
dnotero <otero.diego.87@gmail.com>
2015-03-21 14:26:16 -0300

    add freemarker

---
+++ src/main/java/ar/com/dno/mongodb/HelloWorldFreeMarkerStyle.java
@@ -0,0 +1,27 @@
+package ar.com.dno.mongodb;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class HelloWorldFreeMarkerStyle {
+
+	public static void main(String[] args) throws Exception {
+		Configuration configuration = new Configuration();
+		configuration.setClassForTemplateLoading(HelloWorldFreeMarkerStyle.class, "/");
+		
+		Template template = configuration.getTemplate("hello.ftl");
+		StringWriter writer = new StringWriter();
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("name", "Freemarker");
+		
+		template.process(params, writer);
+		
+		System.out.println(writer);
+	}
+
+}
