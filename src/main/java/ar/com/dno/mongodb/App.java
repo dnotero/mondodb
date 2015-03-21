36a25734b3e3c6ffab97e14561f6c72154431472
dnotero <otero.diego.87@gmail.com>
2015-03-21 12:05:38 -0300

    initial commit

---
+++ src/main/java/ar/com/dno/mongodb/App.java
@@ -0,0 +1,13 @@
+package ar.com.dno.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
